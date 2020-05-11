bdef9f81b6359df80547fe9a36a14b307aba1bec
Leon Carnagie <leonfross@gmail.com>
2020-05-11 13:45:13 -0700

    modified javascript file

--- assets/passwordGen.js
+++ assets/passwordGen.js
@@ -1,47 +1,69 @@
-//Create user-selected criteria
-
-var passwordLength = prompt("how long should the password be")
-var specialChar = confirm("Do you want special Characters")
-var uppercase = confirm("Do you want uppercase characters")
-var lowercase = confirm("Do you want lowercase characters")
-var numbers = confirm("Do you want numberic characters")
 
 // Add event listeners
 
-window.onload = function (){
+window.onload = function () {
     var generateButton = document.querySelector('#generate');
-    generateButton.addEventListener('click', function(){				
+    generateButton.addEventListener('click', function () {
         document.querySelector('#password').value = generate();
 
-            });
+    });
 }
 
 // Add function to create random generator
 
-function generate( length = 12 ){
+function generate() {
     var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     var lowercase = 'abcdefghijklmnopqrstuvwxyz';
     var numbers = '0123456789';
     var specialChar = '!"#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
- 
-    var all = uppercase + lowercase + numbers + specialChar;
- 
-    var password = '';
- 
-    for (var index = 0; index < length; index++) {
-        var character = Math.floor(Math.random() * all.length);
-        password += all.substring(character, character + 1);
+    var passwordLength = prompt("how long should the password be")
+
+
+    var all = "";
+    if (passwordLength < 8 || passwordLength > 128) {
+        alert("Plese enter a number between 8 & 128")
+        return "Invalid Password length - Please enter number between 8 and 128"
     }
- 
-    return password;
- }
+    else {
+        var yesSpecialChar = confirm("Do you want special Characters")
+        var yesUppercase = confirm("Do you want uppercase characters")
+        var yesLowercase = confirm("Do you want lowercase characters")
+        var yesnumbers = confirm("Do you want numberic characters")
+        if (!yesSpecialChar && !yesUppercase && !yesLowercase && !yesnumbers) {
+            alert("please choose atleast one character")
+            return "Please choose atleast one character set"
+        }
+        else {
+            if (yesSpecialChar) {
+                all += specialChar
+            }
+            if (yesUppercase) {
+                all += uppercase
+            }
+            if (yesLowercase) {
+                all += lowercase
+            }
+            if (yesnumbers) {
+                all += numbers
+            }
+            var password = '';
+
+            for (var index = 1; index <= passwordLength; index++) {
+                var character = Math.floor(Math.random() * all.length);
+                password += all.substring(character, character + 1);
+            }
+
+            return password;
+        }
+    }
+}
 
 // Function to copy to clipboard
 
- function copyFunction() {
+function copyFunction() {
     var copyText = document.getElementById("password");
     copyText.select();
-    copyText.setSelectionRange(0, 99999); 
+    copyText.setSelectionRange(0, 99999);
     document.execCommand("copy");
     alert("Copied to clipboard: " + copyText.value);
-  }
+}
